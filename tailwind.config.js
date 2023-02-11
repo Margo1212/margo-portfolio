cd5d9ae353359270bff639cdb4e584ba9b77d4aa
Margo1212 <sirachenko944@gmail.com>
2023-02-11 14:15:26 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx}",   
+  ],
+  theme: {
+    colors: {
+      primary: {
+        
+      },
+      light: {
+        
+      },
+      dark: {
+        
+      },
+      alert: '#AC353D',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
