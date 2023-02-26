e7a37fbc6e3b28dccc2c4bc782d023b0d2b8a0cd
Margo1212 <sirachenko944@gmail.com>
2023-02-26 16:12:23 +0100

    add text

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -1,9 +1,11 @@
+﻿type InputProps = {
+    size?: 'text-3xl' | 'text-6xl' | 'text-2xl' | 'text-xl';
+    weigth: 'normal' | 'medium' | 'bold' | 'semibold';
+    color: 'primary' | 'primary-300' | 'text-dark';
+    children: string;
+  };
 
 
-export const Text = () => {
-    return (
-        <nav>
-           text
-        </nav>
-    )
-}
+  export const Text = ({ size = 'text-3xl', color, weigth, children }: InputProps) => {
+    return <p className={`${size} text-${color} font-${weigth}`} >{children}</p>;
+  };
--- src/views/MainView/MainView.tsx
+++ src/views/MainView/MainView.tsx
@@ -1,9 +1,17 @@
-import { Navigation } from "@components/Navigation";
+import { Text } from "@components/Text";
+
 
 export const MainView = () => {
     return (
         <>
-           <Navigation />
+            <div className="container h-screen px-32 flex items-center">
+                <div className="w-1/2 flex-col">
+                <Text weigth="semibold" color="primary-300" size="text-2xl" >Hi there!</Text>
+                <Text weigth="bold" color="primary" size="text-6xl" >I'm Margo</Text>
+                <Text weigth="semibold" color="primary-300" size="text-2xl" >junior front-end developer</Text>
+                <Text weigth="normal" color="text-dark" size="text-xl" >I love creating beautiful, responsive, user-friendly websites and web applications using modern front-end technologies.</Text>
+                </div>
+            </div>
         </>
     );
 }
