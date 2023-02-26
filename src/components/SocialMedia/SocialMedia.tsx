92e45f2813f13b9d18a54b61da8c9b2624b74f48
Margo1212 <sirachenko944@gmail.com>
2023-02-26 14:09:32 +0100

    add social icons

---
+++ src/components/SocialMedia/SocialMedia.tsx
@@ -0,0 +1,17 @@
+import LinkedIn from '@svg/linkedin.svg';
+import GitHub from '@svg/github.svg';
+import Behance from '@svg/behance.svg';
+import Instagram from '@svg/instagram.svg';
+import Facebook from '@svg/facebook.svg';
+
+export const SocialMedia = () => {
+    return (
+        <ul className="flex">
+            <li className='mx-2'><LinkedIn /></li>
+            <li className='mx-2'><Behance /></li>
+            <li className='mx-2'><Instagram /></li>
+            <li className='mx-2'><Facebook /></li>
+            <li className='mx-2'><GitHub /></li>
+        </ul>
+    )
+}
