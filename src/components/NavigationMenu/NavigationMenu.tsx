fa6c772f6c87d1362e6ccb9525a90d99d5c6c4b1
Margo1212 <sirachenko944@gmail.com>
2023-02-27 12:03:33 +0100

    add links to other pages

--- src/components/NavigationMenu/NavigationMenu.tsx
+++ src/components/NavigationMenu/NavigationMenu.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link"
+
 export const NavigationMenu = () => {
     return (
         <ul className="flex w-1/3 justify-between font-semibold text-base text-primary-300">
-            <li>about</li>
-            <li>portfolio</li>
-            <li>contact</li>
+            <li><Link href="/">about</Link></li>
+            <li><Link href="/projects">portfolio</Link></li>
+            <li><Link href="/contact">contact</Link></li>
         </ul>
     )
 }
