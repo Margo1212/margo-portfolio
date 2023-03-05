f0568bf9490d4ccec6753d007a8ecc502992b524
Margo1212 <sirachenko944@gmail.com>
2023-03-05 20:53:28 +0100

    add storybook and first story💚

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from "@storybook/react"
+
+import { Button } from "./Button"
+
+export default {
+	title: "Button",
+	component: Button,
+	argTypes: {
+		children: {
+			defaultValue: "Zaloguj sie",
+			name: "text inside button",
+			type: {
+				name: "string",
+			},
+		},
+		variant: {
+			options: ["filled", "outline"],
+			control: { type: "radio" },
+			type: {
+				name: "string",
+			},
+		},
+	},
+} as Meta
+
+const Template: Story = (args) => <Button {...args} />
+
+export const BasicButton = Template.bind({})
