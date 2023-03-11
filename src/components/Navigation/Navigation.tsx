210f2efd8614ce8b7bd0f3916816b65b2ff7e939
Margo1212 <sirachenko944@gmail.com>
2023-03-11 18:51:18 +0100

    add mobila view of navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,8 +2,9 @@ import { Language } from "@components/Language/Language"
 import { NavigationMenu } from "@components/NavigationMenu/NavigationMenu"
 import { SocialMedia } from "@components/SocialMedia/SocialMedia"
 import Logo from "@svg/logo.svg"
-import { useEffect, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import { AiOutlineMenu } from "react-icons/ai"
+import { IoClose } from "react-icons/io5"
 
 export const Navigation = () => {
 	const dropdownRef = useRef(null)
@@ -16,50 +17,36 @@ export const Navigation = () => {
 	return (
 		<>
 			<nav
-				className={`relative h-12 px-7 sm:w-screen sm:h-20 sm:px-32 flex justify-between items-center border-[0.1px] border-text-light`}
+				className={`relative h-12 px-7 lg:w-screen lg:h-20 lg:px-32 flex justify-between items-center border-[0.1px] border-text-light`}
 			>
 				<div className="h-10 w-10 flex items-center">
 					<Logo />
 				</div>
 				<div
 					ref={dropdownRef}
-					className={`sm:flex sm:items-center   ${
+					className={` lg:flex lg:w-2/3 lg:justify-between lg:items-center   ${
 						isOpen
-							? "visible absolute z-10 top-[3rem] right-[7px] bg-[#fff] w-34 h-44 p-8 border-[0.1px] border-text-light rounded-lg"
+							? "visible absolute z-10 top-0 right-0 bg-[#fff] w-34 h-44 p-8 border-[0.1px] border-text-light rounded-lg"
 							: "hidden"
 					}`}
 				>
+					<div className="absolute top-0 right-0 w-8 h-8 lg:hidden">
+						<button className="w-[100%] h-[100%]" onClick={openMenu}>
+							<IoClose style={{ color: "purple", width: "100%", height: "100%" }} />
+						</button>
+					</div>
 					<NavigationMenu color="purple" />
-					<div className="hidden sm:flex">
+					<div className="hidden md:flex">
 						<SocialMedia fill="purple" />
 						<Language />
 					</div>
 				</div>
-				<div className="w-8 h-8 sm:hidden">
+				<div className="w-8 h-8 lg:hidden">
 					<button className="w-[100%] h-[100%]" onClick={openMenu}>
 						<AiOutlineMenu style={{ color: "purple", width: "100%", height: "100%" }} />
 					</button>
 				</div>
 			</nav>
-
-			{/* <nav
-					className={` relative `}
-				>
-					
-						<Logo />
-					</div>
-						
-					<div>
-							
-					</div>
-					{/* <NavigationMenu color="purple" />
-					<div className="flex">
-						<SocialMedia fill="purple" />
-						<Language />
-					</div> */}
-			{/* 
-				</nav>
-			)} */}
 		</>
 	)
 }
