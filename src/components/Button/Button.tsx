20a1b2f55cc06944c616a293fe0127db17c69f29
Margo1212 <sirachenko944@gmail.com>
2023-03-11 19:16:14 +0100

    delete button props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,24 +3,22 @@ import { ButtonHTMLAttributes, ReactNode } from "react"
 export type ButtonProps = {
 	children?: ReactNode
 	variant?: string
-	width?: "sm" | "lg"
 } & ButtonHTMLAttributes<HTMLButtonElement>
 
 type ButtonVariants = "filled" | "outline"
 
 const variantClasses: Record<ButtonVariants, string> = {
 	filled:
-		"rounded-full p-2 mr-2 drop-shadow-lg bg-primary-300 text-[#fff] hover:bg-primary-100 active:bg-primary-500",
+		"rounded-full p-2 drop-shadow-lg bg-primary-300 text-[#fff] hover:bg-primary-100 active:bg-primary-500",
 	outline:
-		"rounded-full p-2 mr-2 drop-shadow-lg border-2 bg-[#fff] border-primary-300 text-primary-300  hover:border-primary-100 hover:text-primary-100 active:border-primary-500 active:text-primary-500",
+		"rounded-full p-2 drop-shadow-lg border-2 bg-[#fff] border-primary-300 text-primary-300  hover:border-primary-100 hover:text-primary-100 active:border-primary-500 active:text-primary-500",
 }
 
 export const Button = (props: ButtonProps) => {
-	const { children, variant, width } = props
-	const btnWidth = width === "sm" ? "w-36" : "w-1/2"
+	const { children, variant } = props
 	return (
 		<button
-			className={`${variantClasses[variant as keyof Record<ButtonVariants, string>]} ${btnWidth}`}
+			className={`${variantClasses[variant as keyof Record<ButtonVariants, string>]} lg:w-36`}
 			{...props}
 		>
 			{children}
